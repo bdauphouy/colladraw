c78f224eb2e7ae505a9344dcbd9ffeca8181ca33
Arthur Fontaine <0arthurfontaine@gmail.com>
2022-03-17 16:12:03 +0100

    feat: add a method "getCoordinates" for the Triangle

--- client/src/scripts/colladraw/src/lib/shapes/Triangle.ts
+++ client/src/scripts/colladraw/src/lib/shapes/Triangle.ts
@@ -10,4 +10,12 @@ export default class Triangle extends Polygon {
     this.sidesNumber = 3;
     this.polygonName = "Triangle";
   }
+
+  getCoordinates(startX: number = this.x, startY: number = this.y): number[][] {
+    return [
+      [startX, startY],
+      [startX + this.width, startY],
+      [startX + this.width / 2, startY + this.height]
+    ];
+  }
 }
